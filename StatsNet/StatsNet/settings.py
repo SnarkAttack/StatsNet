d946733a1a0bb7078b4d2a0bde5846710e825708
Patrick McQuay <patrick@Patricks-MacBook-Pro.local>
2017-08-02 00:24:06 -0400

    Added to .gitignore and edited settings for correct testing database

--- StatsNet/StatsNet/settings.py
+++ StatsNet/StatsNet/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'StatsNet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'netrunner',
+	'USER': 'netrunneruser',
+	'PASSWORD': 'netrunner',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
